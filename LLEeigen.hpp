060e3805c659cf36f7a1460cee89c731381af9c1
xiangze <xiangze@gmail.com>
2012-02-20 23:17:39 +0900

    The direction inequility of is corrected

--- LLEeigen.hpp
+++ LLEeigen.hpp
@@ -1,27 +1,36 @@
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/SVD>
 #include <eigen3/Eigen/LU>
-#ifdef _SPRARCE
+#ifdef _SPARSE
+#define EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET
 #include <eigen3/Eigen/Sparse>
 #include <redsvd/redsvd.hpp>
 #endif
+
+#include "iostream"
 
 using namespace Eigen;
 
 #define DBL_MAX (100000)
 #define MINVAL (.00001)
 
-//d; distance matrix
-//out:
-//n: neigher num
+//data :input data 
+//d; distance matrix of data
+//out: output (dimenstion compressed expression of data)
+//n: neigher number
 void lle_eigen(MatrixXf & dist,MatrixXf & data,MatrixXf & out,
-	       int dim,int nnum){
+	       int nnum){
+#ifdef _SPARSE
+SparseMatrix<float>  W(dist.rows(), dist.cols());
+  W.setZero();
+#else
+  MatrixXf  W= MatrixXf::Zero(dist.rows(), dist.cols());
+#endif
 
-  MatrixXf  W= MatrixXf::Zero(data.rows(), dist.cols());//square matrix
   VectorXf::Index m;
   VectorXf vd;
   for (int i = 0;i < data.rows();i++){
-    // 隣接行列
+    // neigherhood matrix
     MatrixXf  nm(nnum,data.cols());
 
     vd=dist.row(i);
@@ -39,7 +48,7 @@ void lle_eigen(MatrixXf & dist,MatrixXf & data,MatrixXf & out,
       if(nnum>data.cols())
 	lc+=MINVAL*MatrixXf::Identity(lc.rows(),lc.cols());
 
-      VectorXf v1(lc.rows(),1.0);
+      VectorXf v1=VectorXf::Constant(lc.rows(),1.0);
       FullPivLU<MatrixXf> lu(lc);
       VectorXf vw=lu.solve(v1);//lc*vw=v1
 
@@ -50,25 +59,43 @@ void lle_eigen(MatrixXf & dist,MatrixXf & data,MatrixXf & out,
 
       for (int r=0;r<vw.size();r++){
 	vd.minCoeff(&m);
+#ifdef _SPARSE
+	W.insert(i, m) = vw(r)/wsum;
+#else
 	W(i, m) = vw(r)/wsum;
+#endif
 	vd(m) = DBL_MAX;
       }
   }
+  //  std::cout << "W:" << W << std::endl;
 
-  MatrixXf sm = -W;
-  sm+= MatrixXf::Identity(sm.rows(),sm.cols());
+#ifdef _SPARSE
+  SparseMatrix<float>sm = -W;
+
+  for (int r = 0;r < sm.rows();r++)
+    sm.insert(r,r)= sm(r,r)+1;
 
   sm = sm.transpose()*sm;
-#ifdef _SPRARSE
+
+  REDSVD::RedSVD redSVD(sm, out.cols()+1);
+
+  // for (int r = 0;r < out.rows();r++)
+  //   for (int c = 0;c < out.cols();c++)
+  //     out(r, c) = evecs(r,c +1);
 
 #else
+  MatrixXf sm = -W;
+  sm+= MatrixXf::Identity(sm.rows(),sm.cols());
+  sm = sm.transpose()*sm;
+  //  std::cout << "sm:" << sm << std::endl;
+
   SelfAdjointEigenSolver<MatrixXf> eigensolver(sm);
   MatrixXf evecs=eigensolver.eigenvectors();
-#endif
 
   for (int r = 0;r < out.rows();r++)
     for (int c = 0;c < out.cols();c++)
-      out(r, c) = evecs(r, evecs.cols() - c - 2);
+      out(r, c) = evecs(r,c +1);
+#endif
 
   return;
 }
